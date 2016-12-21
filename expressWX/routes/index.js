ee7b4cd2e82961d61ee33d2b0276138f04623cf6
youxunlin <youxun.lin@360gst.com>
2016-12-21 18:02:08 +0800

    修改参数接收方式

--- expressWX/routes/index.js
+++ expressWX/routes/index.js
@@ -7,7 +7,8 @@ var mg = require("../tools/mongoose");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  mg.saveUser({"openid":"12312312312"})
+  console.log(req.headers);
+  // mg.saveUser({"openid":"12312312312"})
   res.render('index', { title: 'Express' });
 });
 
@@ -19,5 +20,79 @@ router.get('/wx.do', function(req, res, next) {
   }
   res.render('index', { title: 'Express' });
 });
+
+router.post("/citylist",function(req,res,next){
+  let postData = "";
+  req.on("data",function(data){
+    postData+=data
+  })
+  req.on("end",function(){
+    console.log(postData);
+    return res.send(postData);
+  })
+  // return res.send({data:"sss"})
+})
+
+router.get("/citylist",function(req,res,next){
+  const cityList = {
+     "city_list" : [
+      {
+        "city_id" : "762",
+        "city_name" : "test",
+        "city_no" : 762,
+        "province_no" : "44"
+      },
+      {
+        "city_id" : "761",
+        "city_name" : "福州",
+        "city_no" : 761,
+        "province_no" : "35"
+      },
+      {
+        "city_id" : "0760",
+        "city_name" : "中山",
+        "city_no" : 760,
+        "province_no" : "44"
+      },
+      {
+        "city_id" : "010",
+        "city_name" : "北京",
+        "city_no" : 10,
+        "province_no" : "11"
+      },
+      {
+        "city_id" : "020",
+        "city_name" : "广州",
+        "city_no" : 20,
+        "province_no" : "44"
+      },
+      {
+        "city_id" : "0510",
+        "city_name" : "无锡",
+        "city_no" : 510,
+        "province_no" : "32"
+      },
+      {
+        "city_id" : "0755",
+        "city_name" : "深圳",
+        "city_no" : 755,
+        "province_no" : "44"
+      },
+      {
+        "city_id" : "0757",
+        "city_name" : "佛山",
+        "city_no" : 757,
+        "province_no" : "44"
+      }
+    ],
+    "city_list_num" : 8,
+    "current_page" : 1,
+    "message" : "查询城市列表成功",
+    "status" : 0,
+    "total_num" : 8,
+    "total_page" : 1
+  }
+  return res.send(cityList);
+})
 
 module.exports = router;
--- expressWX/tools/mongoose.js
+++ expressWX/tools/mongoose.js
@@ -1,40 +1,40 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/wx');
-
-var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
-
-
-/**
- *  微信
- */
- var WXAccessToken = new Schema({
-   access_token:String,
-   expires_in:Number,
-   create_date:Date,
-   end_date:Date
- });
- WXAccessToken
-
-
-
-/**
- * [WXUser description]
- * @type {Schema}
- */
-var WXUser = new Schema({
-    openid      : String
-});
-
-var WXUserModel = mongoose.model('WXUser',WXUser);
-
-function saveUser(user){
-  var wxUser = new WXUserModel(user);
-  wxUser.save(function (err) {
-    if (!err) console.log('Success!');
-  });
-}
-
-module.exports = {
-  saveUser:saveUser
-}
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/wx');
+//
+// var Schema = mongoose.Schema,
+//     ObjectId = Schema.ObjectId;
+//
+//
+// /**
+//  *  微信
+//  */
+//  var WXAccessToken = new Schema({
+//    access_token:String,
+//    expires_in:Number,
+//    create_date:Date,
+//    end_date:Date
+//  });
+//  WXAccessToken
+//
+//
+//
+// /**
+//  * [WXUser description]
+//  * @type {Schema}
+//  */
+// var WXUser = new Schema({
+//     openid      : String
+// });
+//
+// var WXUserModel = mongoose.model('WXUser',WXUser);
+//
+// function saveUser(user){
+//   var wxUser = new WXUserModel(user);
+//   wxUser.save(function (err) {
+//     if (!err) console.log('Success!');
+//   });
+// }
+//
+// module.exports = {
+//   saveUser:saveUser
+// }
